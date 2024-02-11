83041673fa4d064877f36daa0cffb8f2b6e8b5e6
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-02-11 09:46:05 +0000

    add files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: "standard-with-typescript",
-    overrides: [
-      {
-        env: {
-          node: true,
-        },
-        files: [".eslintrc.{js,cjs}"],
-        parserOptions: {
-          sourceType: "script",
-        },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "standard-with-typescript",
+  overrides: [
+    {
+      env: {
+        node: true,
       },
-    ],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    rules: {
-      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    },
-  };
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+};
